017f0012c8e24cac924412f4a70f8471dd273e8c
Armel Wonga <armel.g.ngassa@studmail.w-hs.de>
2021-01-04 17:23:47 +0100

    update the canvas.js and  index.html

--- canvas.js
+++ canvas.js
@@ -1,51 +1,52 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
     const canvas = document.querySelector("#canvas");
     const context = canvas.getContext("2d");
-    
+
     // Resizing
-    canvas.heighr = window.innerHeight;
+    canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
 
-    context.strokeStyle = "red";
-    context.lineWidth = 5;
-    context.fillRect(50,50, 200, 200);
-    context.strokeRect(50,50, 200, 200);
+    // context.strokeStyle = "red";
+    // context.lineWidth = 5;
+    // context.fillRect(50, 50, 200, 200);
+    // context.strokeRect(50, 50, 200, 200);
 
 
-    context.beginPath();
-    context.moveTo(100, 100);
-    context.lineTo(200, 100);
-    context.lineTo(200, 150);
-    context.closePath();
-    context.stroke();
-    
+    // context.beginPath();
+    // context.moveTo(100, 100);
+    // context.lineTo(200, 100);
+    // context.lineTo(200, 150);
+    // context.closePath();
+    // context.stroke();
+
     // variables
     let painting = false;
 
-    function startPosition(e){
+    function startPosition(e) {
         painting = true;
         draw(e);
     }
-    function finishedPosition(){
+    function finishedPosition() {
         painting = false;
         context.beginPath();
     }
-    function draw(e){
-        if(!painting){
-            return;
-        }
+    function draw(e) {
+        if (!painting) return;
         context.lineWidth = 10;
-        context.lineCap="round";
+        context.lineCap = "round";
+        context.strokeStyle = "red";
 
         context.lineTo(e.clientX, e.clientY);
         context.stroke();
         context.beginPath();
         context.moveTo(e.clientX, e.clientY);
 
+
     }
     // EventListerners
-    canvas.addEventListener("mousedown",startPosition);
-    canvas.addEventListener("mouseup")
-})
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mousedown", finishedPosition);
+    canvas.addEventListener("mouseup", draw);
+});
 
 //window.addEventListener(resi)
